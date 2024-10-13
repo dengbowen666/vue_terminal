17fcad031cadab2f11582597a90fd22673314c17
dengbowen666 <2763201730@qq.com>
2024-10-13 22:26:59 +0800

    $commit_message

--- push.bat
+++ push.bat
@@ -1,11 +1,16 @@
+#!/bin/bash
 
-# 获取当前时间，格式为 "YYYY-MM-DD HH:MM:SS"
+# 获取当前时间
 current_time=$(date "+%Y-%m-%d %H:%M:%S")
 
-# 检查是否有未提交的更改
-  # 添加所有更改到暂存区
-  git add .
-  # 提交更改，使用当前时间作为提交信息
-  git commit -m "$current_time"
-  # 推送到远程仓库，默认为origin分支
-  git push origin master
+# 提交信息
+commit_message="Update at $current_time"
+
+# 添加所有更改到暂存区
+git add .
+
+# 提交更改
+git commit -m "$commit_message"
+
+# 推送到远程仓库
+git push origin main
